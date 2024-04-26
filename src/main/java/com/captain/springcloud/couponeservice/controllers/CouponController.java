facccc2f4c1b2cf79c0eefc5f64208f5d19769d1
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-26 11:14:55 +0300

    add thymeleaf dependancy and html templates

---
+++ src/main/java/com/captain/springcloud/couponeservice/controllers/CouponController.java
@@ -0,0 +1,41 @@
+package com.captain.springcloud.couponeservice.controllers;
+
+import com.captain.springcloud.couponeservice.model.Coupon;
+import com.captain.springcloud.couponeservice.repository.CouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CouponController {
+
+    @Autowired
+    private CouponRepository repo;
+
+    @GetMapping("/showCreateCoupon")
+    public String showCreateCoupon() {
+        return "createCoupon";
+    }
+
+    @PostMapping("/saveCoupon")
+    public String save(Coupon coupon) {
+        repo.save(coupon);
+        return "createResponse";
+    }
+
+    @GetMapping("/showGetCoupon")
+    public String showGetCoupon() {
+        return "getCoupon";
+    }
+
+    @PostMapping("/getCoupon")
+    public ModelAndView getCoupon(String code) {
+        ModelAndView mav = new ModelAndView("couponDetails");
+        System.out.println(code);
+        mav.addObject(repo.findByCode(code));
+        return mav;
+    }
+
+}
