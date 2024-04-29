36eed4903b46be425148a50c94f7ba33d786d2a0
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-29 11:35:38 +0300

    implement custom login

--- src/main/java/com/captain/springcloud/couponeservice/config/WebSecurityConfig.java
+++ src/main/java/com/captain/springcloud/couponeservice/config/WebSecurityConfig.java
@@ -1,19 +1,35 @@
 package com.captain.springcloud.couponeservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
     }
 
     @Bean
---
+++ src/main/java/com/captain/springcloud/couponeservice/security/SecurityService.java
@@ -0,0 +1,7 @@
+package com.captain.springcloud.couponeservice.security;
+
+public interface SecurityService {
+
+    boolean login(String username, String password);
+
+}
